b8edfeede4df3dd02a17e9b577c7556a77f4ac4d
Joshua Eklund <eklun124@sentinel.morris.umn.edu>
2021-08-30 21:59:35 -0500

    I did the thing

--- count_successes.sh
+++ count_successes.sh
@@ -1,3 +1,18 @@
-#!/usr/bin/bash
+#!/usr/bin/env bash
 
-  ls test_data
+  tar_file=$1
+
+  SCRATCH=$(mktemp --directory)
+  
+  # z means working with compressed tar file
+  # x means extract contents
+  # f means which file we are extracting
+  # --directory specifies which the directory the files should go in 
+  tar zxf "$tar_file" --directory "$SCRATCH"
+
+  num_successes=$(grep -lr SUCCESS "$SCRATCH" | wc -l)
+  num_failures=$(grep -lr FAILURE "$SCRATCH" | wc -l)
+
+  echo "There were $num_successes successes and $num_failures."
+
+  rm -rf "$SCRATCH"
